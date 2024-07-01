df13c822206dae43f63afd707118bfb9f2b72af6
Yasser <fahmiyasser03@gmail.com>
2024-07-01 14:41:32 +0100

    Adding Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 export default function Footer() {
+    const links = [
+        'Contact', 'Mentions légales', 'Vie Privée et Données Personnelles', 'Politique des cookies', 'Entreprise Today', 'Nos publications', 'Plan du site', 'Annuaire', 'Accessibilité : conformité partielle', 'Code de Conduite'
+    ]
     return (
         <div className="Footer">
             <div className="top">
@@ -23,6 +27,14 @@ export default function Footer() {
                         <a href='#'>Haut de la page <i className="fa-solid fa-arrow-up"></i></a>
                     </div>
                 </div>
+            </div>
+            <div className="buttom">
+                <div className="links">
+                    {links.map((item, index) => (
+                        <Link key={index}>{item}</Link>
+                    ))}
+                </div>
+                <p>© Entreprise 2004-2024 - Tous droits réservés</p>
             </div>
         </div>
     )
